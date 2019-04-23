d3cd952c0513d770d23658d0eaf587be0034e75c
Corentin Henry <corentinhenry@gmail.com>
2019-04-23 15:28:24 +0200

    elisp/hamming: handle mentor's comment

--- exercism/elisp/hamming/hamming.el
+++ exercism/elisp/hamming/hamming.el
@@ -5,20 +5,19 @@
 ;;; Code:
 
 (defun hamming-distance (s1 s2)
-  "Return the hamming distance between S1 and S2. The two given
-sequences must have the same length, oftherwise, this function
-returns an error."
+  "Return the hamming distance between S1 and S2.
+The two given sequences must have the same length, oftherwise,
+this function returns an error."
   
   (when (/= (length s1) (length s2))
     throw "The two sequences don't have the same length.")
   
   (let
-      ((initial-diff 0)
-       (pairs (seq-mapn #'cons s1 s2))
+      ((pairs (seq-mapn #'cons s1 s2))
        (f (lambda (accumulated-diff pair)
             (if (equal (car pair) (cdr pair))
                 accumulated-diff
               (+ accumulated-diff 1)))))
-    (seq-reduce f pairs initial-diff)))
+    (seq-reduce f pairs 0)))
 (provide 'hamming)
 ;;; hamming.el ends here
