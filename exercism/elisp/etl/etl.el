37da1b939e0e2057e23f7515cf66a192430e3823
Corentin Henry <corentinhenry@gmail.com>
2019-05-21 18:36:35 +0200

    elisp/etl: add solution

---
+++ exercism/elisp/etl/etl.el
@@ -0,0 +1,24 @@
+;;; etl.el --- etl Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+(defun etl (hash-table)
+  (let ((new-hash-table (make-hash-table)))
+    (maphash
+     (lambda (score letters)
+       (if (< score 0)
+           (error "Negative score")
+         (cl-loop for letter in letters
+                  do (progn
+                       (when (not (char-or-string-p letter))
+                         (error "Invalid value (not a string)"))
+                       (when (not (equal (length letter) 1))
+                         (error (format "Invalid value: expected a letter, got %s" letter)))
+                       (puthash (downcase letter) score new-hash-table)))))
+     hash-table)
+    new-hash-table))
+
+(provide 'etl)
+;;; etl.el ends here
