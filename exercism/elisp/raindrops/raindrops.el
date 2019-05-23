23f626ba1f1ee8031f9b479e86f236b18422bb4b
Corentin Henry <corentinhenry@gmail.com>
2019-05-23 10:50:49 +0200

    elisp/raindrops: simplify the solution with concat
    this works because `concat` handle `nil` like the empty string:
    
    ELISP> (concat nil "a" nil "b")
    "ab"
    
    this was taken from xuchunyang's solution on this exercice
    https://exercism.io/tracks/elisp/exercises/raindrops/solutions/1bcecb5b3b214b879768ecc2ab088d98

--- exercism/elisp/raindrops/raindrops.el
+++ exercism/elisp/raindrops/raindrops.el
@@ -6,15 +6,12 @@
 
 (defun convert (n)
   "Convert integer N to its raindrops string."
-  (cond
-   ((zerop (mod n (* 3 5 7))) "PlingPlangPlong")
-   ((zerop (mod n (* 3 7))) "PlingPlong")
-   ((zerop (mod n (* 3 5))) "PlingPlang")
-   ((zerop (mod n (* 5 7))) "PlangPlong")
-   ((zerop (mod n (* 3))) "Pling")
-   ((zerop (mod n (* 5))) "Plang")
-   ((zerop (mod n (* 7))) "Plong")
-   (t (format "%d" n))))
+  (let ((result (concat (and (zerop (mod n 3)) "Pling")
+                        (and (zerop (mod n 5)) "Plang")
+                        (and (zerop (mod n 7)) "Plong"))))
+    (if (string= "" result)
+        (format "%d" n)
+      result)))
 
 (provide 'raindrops)
 ;;; raindrops.el ends here
