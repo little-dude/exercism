4f879939c677dc6c8e4cfe5cbbf64e49486a7c49
Corentin Henry <corentinhenry@gmail.com>
2019-05-23 10:57:26 +0200

    elisp/raindrops: simplify again with `pcase`
    again taken from xuchunyang's solution

--- exercism/elisp/raindrops/raindrops.el
+++ exercism/elisp/raindrops/raindrops.el
@@ -6,12 +6,11 @@
 
 (defun convert (n)
   "Convert integer N to its raindrops string."
-  (let ((result (concat (and (zerop (mod n 3)) "Pling")
-                        (and (zerop (mod n 5)) "Plang")
-                        (and (zerop (mod n 7)) "Plong"))))
-    (if (string= "" result)
-        (format "%d" n)
-      result)))
+  (pcase (concat (and (zerop (mod n 3)) "Pling")
+                 (and (zerop (mod n 5)) "Plang")
+                 (and (zerop (mod n 7)) "Plong"))
+    ("" (format "%d" n))
+    (s s)))
 
 (provide 'raindrops)
 ;;; raindrops.el ends here
