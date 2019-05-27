6b5bfbba6d674c3e8de5f2a00d97de08728df109
Corentin Henry <corentinhenry@gmail.com>
2019-05-27 08:40:17 +0200

    elisp/leap: simplify the solution even more

--- exercism/elisp/leap/leap.el
+++ exercism/elisp/leap/leap.el
@@ -8,8 +8,7 @@
   "Return `t` if YEAR is a leap year, `nil` otherwise."
   (and (zerop (% year 4))
        (or (not (zerop (% year 100)))
-           (and (zerop (% year 100))
-                (zerop (% year 400))))))
+           (zerop (% year 400)))))
 
 (provide 'leap)
 ;;; leap.el ends here
