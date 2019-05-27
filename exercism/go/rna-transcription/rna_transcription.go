eb60878ef127e4b7a3317e4af9bfc3e49ba21a19
Corentin Henry <corentinhenry@gmail.com>
2019-05-27 16:08:53 +0200

    go/rna_transcription: add some notes about various other solutions

--- exercism/go/rna-transcription/rna_transcription.go
+++ exercism/go/rna-transcription/rna_transcription.go
@@ -20,3 +20,55 @@ func ToRNA(dna string) string {
 	}
 	return result.String()
 }
+
+// benchmark results:
+//
+// BenchmarkRNATranscription-8   	10000000	       146 ns/op	      16 B/op	       5 allocs/op
+// PASS
+// ok  	github.com/little-dude/exercism/go/rna-transcription	1.633s
+//
+// Note that there is a simpler solution with strings.Replacer as
+// found in
+// https://exercism.io/tracks/go/exercises/rna-transcription/solutions/5fbbe4d003222e82656c3834
+//
+// var replacer = strings.NewReplacer(
+// 	"G", "C",
+// 	"C", "G",
+// 	"T", "A",
+// 	"A", "U")
+//
+// func ToRNA(input string) string {
+// 	return replacer.Replace(input)
+// }
+//
+// However benchmarks show that this is slower:
+//
+// BenchmarkRNATranscription-8   	10000000	       221 ns/op	      64 B/op	       6 allocs/op
+// PASS
+// ok  	github.com/little-dude/exercism/go/rna-transcription	2.468s
+//
+// More surprisingly, the solution with a simple byte buffer from
+// https://exercism.io/tracks/go/exercises/rna-transcription/solutions/16da121be28d43e89ee1e8ae881b44e8
+// is also slower, I'm not sure why, because afaict, there's also a
+// single allocation when we make the byte buffer (unless the
+// conversion to a string also allocates??)
+//
+// var translate = map[rune]byte{
+// 	'G': 'C',
+// 	'C': 'G',
+// 	'T': 'A',
+// 	'A': 'U',
+// }
+//
+// // ToRNA - return DNA compliment
+// func ToRNA(dna string) string {
+// 	rna := make([]byte, len(dna))
+// 	for i, c := range dna {
+// 		rna[i] = translate[c]
+// 	}
+// 	return string(rna)
+// }
+//
+// BenchmarkRNATranscription-8   	10000000	       191 ns/op	      32 B/op	       6 allocs/op
+// PASS
+// ok  	github.com/little-dude/exercism/go/rna-transcription	2.129s
