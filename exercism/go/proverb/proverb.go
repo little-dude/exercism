00e8b597017fe9f60ef56c7b9af3ef3b6fd51506
Corentin Henry <corentinhenry@gmail.com>
2019-06-15 16:59:29 +0200

    go/proverb: style nit

--- exercism/go/proverb/proverb.go
+++ exercism/go/proverb/proverb.go
@@ -11,11 +11,11 @@ const (
 
 // Proverb returns the proverb's sentences for the given words.
 func Proverb(rhyme []string) []string {
-	result := make([]string, 0, len(rhyme))
 	if len(rhyme) < 1 {
-		return result
+		return []string{}
 	}
 
+	result := make([]string, 0, len(rhyme))
 	previous := rhyme[0]
 	for _, s := range rhyme[1:] {
 		result = append(result, fmt.Sprintf(stanza, previous, s))
