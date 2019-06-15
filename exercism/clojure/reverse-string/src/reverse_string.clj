3780456712834f08f3081672a01bbf2424e2a6dd
Corentin Henry <corentinhenry@gmail.com>
2019-06-15 20:23:41 +0200

    clojure/reverse-string: simplify the solution

--- exercism/clojure/reverse-string/src/reverse_string.clj
+++ exercism/clojure/reverse-string/src/reverse_string.clj
@@ -2,7 +2,14 @@
 
 (defn reverse-string [s]
   "Reverse the given string `s`."
-  (let
-      [char-seq (seq s)
-       reversed-char-seq '()]
-    (clojure.string/join (reduce conj reversed-char-seq char-seq))))
+  ; this relis on the fact that for lists,
+  ;
+  ;      `(conj my-list my-scalar)`
+  ;
+  ; prepends `my-scalar` to `my-list`. So a solution could be:
+  ;
+  ;      `(apply conj nil (seq s))`
+  ;
+  ; But `(apply conj) is basically equivalent to `into`.
+  ; See: https://www.braveclojure.com/core-functions-in-depth/#The_Collection_Abstraction
+  (clojure.string/join (into nil (seq s))))
