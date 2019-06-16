af2ea4d0c48aeb1387fb264459532c2c475bce12
Corentin Henry <corentinhenry@gmail.com>
2019-06-16 12:15:03 +0200

    clojure/collatz: add solution with reduce

--- exercism/clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ exercism/clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -9,3 +9,15 @@
         (= value 1) steps
         (even? value)  (recur (/ value 2) (inc steps))
         :else (recur (inc (* 3 value)) (inc steps))))))
+
+;; I initially wanted to use `reduce` but didn't know how to exit the loop.
+;; The solution is to use `reduced`, as shown in this solution:
+;; https://exercism.io/tracks/clojure/exercises/collatz-conjecture/solutions/a766f26a685a4f408d84631163c9941b
+;;
+;; (defn collatz [n]
+;;   (if (< n 1)
+;;     (throw (IllegalArgumentException. (str "invalid n: " n)))
+;;     (reduce (fn [n i] (cond (= n 1)  (reduced i) ; reduced is used to exit
+;;                             (odd? n) (inc (* n 3))
+;;                             :else    (/ n 2)))
+;;             n (range))))
