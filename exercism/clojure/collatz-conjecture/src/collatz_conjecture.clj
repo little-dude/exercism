a4694b90e332806a9461904ed2c42ef4f1ec6e37
Corentin Henry <corentinhenry@gmail.com>
2019-06-16 12:09:26 +0200

    clojure/collatz: simplify `loop`

--- exercism/clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ exercism/clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -5,10 +5,7 @@
     (throw (Exception. "expected a strictly positive integer."))
     (loop [value num
            steps 0]
-      (if (= value 1)
-        steps
-        (let [value (if (zero? (mod value 2))
-                      (/ value 2)
-                      (inc (* 3 value)))
-              steps (inc steps)]
-          (recur value steps))))))
+      (cond
+        (= value 1) steps
+        (even? value)  (recur (/ value 2) (inc steps))
+        :else (recur (inc (* 3 value)) (inc steps))))))
