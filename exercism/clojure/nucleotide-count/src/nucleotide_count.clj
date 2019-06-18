d02fb5b039ca537cb8f5db46140a3c8df12498fc
Corentin Henry <corentinhenry@gmail.com>
2019-06-18 16:48:08 +0200

    clojure/nucleotide-count: move validation logic in special forms
    :pre and :post are very nice, see:
    https://exercism.io/tracks/clojure/exercises/nucleotide-count/solutions/6b6fd15977f74567a2235c3877e9694f
    
    also to change things a bit, do not use `reduce` in
    nucleotide-counts. The trick with `merge` is pretty neat and I've seen
    it in multiple solutions, so it seems quite idiomatic.

--- exercism/clojure/nucleotide-count/src/nucleotide_count.clj
+++ exercism/clojure/nucleotide-count/src/nucleotide_count.clj
@@ -1,19 +1,17 @@
 (ns nucleotide-count)
 
-;; count exists of course, but part of the exercise to re-implement it
+(def valid-nucleotides #{\A \C \G \T})
+
 (defn count [nucleotide strand]
-  (if (not (contains? #{\A \C \G \T} nucleotide))
-    (throw (Exception. "invalid nucleotide"))
-    (reduce (fn [counter item] (if (= item nucleotide) (inc counter) counter))
-            0
-            strand)))
+  {:pre [(contains? valid-nucleotides nucleotide)]}
+  (reduce
+   (fn [counter item]
+     (if (= item nucleotide) (inc counter) counter))
+   0
+   strand))
 
 (defn nucleotide-counts [strand]
-  (reduce (fn [counts nucleotide]
-            (if (contains? counts nucleotide)
-              ;; I initially used `assoc`, but `update` is a better fit
-              ;; (assoc counts nucleotide (inc nucleotide counts))
-              (update counts nucleotide inc)
-              (throw (Exception. "invalid nucleotide"))))
-          {\A 0 \C 0 \T 0 \G 0}
-          strand))
+  {:post [(= valid-nucleotides (set (keys %)))]}
+  (merge
+   (zipmap valid-nucleotides (repeat 0))
+   frequencies strand))
