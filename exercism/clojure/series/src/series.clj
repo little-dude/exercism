9a13ef10bb4fe7a7410f85e0c60d818ea674c400
Corentin Henry <corentinhenry@gmail.com>
2019-06-18 16:16:28 +0200

    Revert "clojure/series: do not re-implement partition"
    This reverts commit d7aa077788d77523ebda5fe6faedd1883a0cc33f.
    
    The point of the exercise is probably to re-implement it so let's keep
    it.

--- exercism/clojure/series/src/series.clj
+++ exercism/clojure/series/src/series.clj
@@ -3,4 +3,17 @@
 (defn slices [string length]
   (if (zero? length)
     [""]
-    (map (partial apply str) (partition length 1 string))))
+    (map (partial apply str) (sliding-window string length))))
+
+;; note that for the exercise we implemented this function, but
+;; `partition` already does that
+(defn sliding-window [seq length]
+  (if (<= length 0)
+    nil
+    (loop [result ()
+           remaining seq]
+      (let [chunk (take length remaining)]
+        (if (= (count chunk) length)
+          (recur (cons chunk result) (rest remaining))
+          (if (not (empty? result))
+            (reverse result)))))))
