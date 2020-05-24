ac8f4648c0febccc66b7fefb00895fab79eb48b3
little-dude <corentinhenry@gmail.com>
2020-05-24 09:36:49 +0200

    elixir: secret handshake: use Enum.reduce
    this is probably a bit less readable though, but there was some
    repetition in the previous version that bothered me.

--- elixir/secret-handshake/lib/secret_handshake.ex
+++ elixir/secret-handshake/lib/secret_handshake.ex
@@ -1,6 +1,14 @@
 defmodule SecretHandshake do
   use Bitwise
 
+  @actions [
+    {1 <<< 3, "jump"},
+    {1 <<< 2, "close your eyes"},
+    {1 <<< 1, "double blink"},
+    {1 <<< 0, "wink"},
+    {1 <<< 4, &Enum.reverse/1}
+  ]
+
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -17,20 +25,16 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
-    []
-    |> do_commands(has_action_code?(code, 0x08), "jump")
-    |> do_commands(has_action_code?(code, 0x04), "close your eyes")
-    |> do_commands(has_action_code?(code, 0x02), "double blink")
-    |> do_commands(has_action_code?(code, 0x01), "wink")
-    |> do_commands(has_action_code?(code, 0x10), &Enum.reverse/1)
+    reducer = fn {action_code, action}, acc ->
+      do_command(acc, do?(code, action_code), action)
+    end
+
+    Enum.reduce(@actions, [], reducer)
   end
 
-  defp do_commands(acc, do?, todo)
-  defp do_commands(acc, false, _), do: acc
-  defp do_commands(acc, true, str) when is_binary(str), do: [str | acc]
-  defp do_commands(acc, true, fun), do: fun.(acc)
+  defp do_command(acc, false, _), do: acc
+  defp do_command(acc, true, str) when is_binary(str), do: [str | acc]
+  defp do_command(acc, true, fun), do: fun.(acc)
 
-  defp has_action_code?(code, action_code) do
-    action_code == (code &&& action_code)
-  end
+  defp do?(code, action_code), do: action_code == (code &&& action_code)
 end
