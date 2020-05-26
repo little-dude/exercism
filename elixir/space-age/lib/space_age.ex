1f581e107a3f05301600d78b7ac3690b5b9b6013
little-dude <corentinhenry@gmail.com>
2020-05-26 17:23:05 +0200

    elixir: space-age initial solution

---
+++ elixir/space-age/lib/space_age.ex
@@ -0,0 +1,33 @@
+defmodule SpaceAge do
+  @type planet ::
+          :mercury
+          | :venus
+          | :earth
+          | :mars
+          | :jupiter
+          | :saturn
+          | :uranus
+          | :neptune
+
+  @coefficients %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1.0,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+
+  @seconds_in_earth_year 31_557_600
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(planet, seconds) do
+    seconds / (@coefficients[planet] * @seconds_in_earth_year)
+  end
+end
